eb9137c84a1749d78623e4835a2b0f560eef6bb9
Aakarshanj05 <aakarshandatabse05@gmail.com>
2024-01-11 22:55:43 +0530

    Project almost done

---
+++ src/exception/__init__.py
@@ -0,0 +1,29 @@
+import sys
+import traceback
+
+class CustomException(Exception):
+    def __init__(self, error_message: Exception, error_details: type):
+        self.error_message = CustomException.get_detailed_error_message(
+            error_message=error_message, 
+            error_details=error_details
+        )
+
+    @staticmethod
+    def get_detailed_error_message(error_message: Exception, error_details: type) -> str:
+        _, _, exc_tb = sys.exc_info()
+
+        exception_block_line_number = exc_tb.tb_lineno
+        file_name = exc_tb.tb_frame.f_code.co_filename
+
+        error_message = (
+            f"Error occurred in script: [{file_name}] at "
+            f"line number: [{exception_block_line_number}] "
+            f"with error message: [{error_message}]"
+        )
+        return error_message
+
+    def __str__(self):
+        return self.error_message
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.error_message})"
